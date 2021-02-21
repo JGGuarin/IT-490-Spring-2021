79b161a0daf1e0043632d1cd815966b06617b112
Josef Guarin <josef.guarin@gmail.com>
2021-02-21 18:22:40 -0500

    Added logger.php file and created a modified testServer, not tested locally yet

---
+++ logger.php
@@ -0,0 +1,49 @@
+#!/usr/bin/php
+<?php
+require_once('rabbitMQLib.inc');
+
+// Will be used for a centralized logger
+class CentralizedLogger
+{
+    public $logFile;
+
+    // Automatically called when creating an object from a class
+    function __construct()
+    {
+        // Creates the log file if it doesn't exist and appends to it
+        $logFile = fopen("centralizedLog.log", "a");
+        echo "centralizedLogger BEGIN".PHP_EOL;
+        fwrite($logFile, "centralizedLogger BEGIN".PHP_EOL);
+    
+        $logServer = new rabbitMQServer("testRabbitMQ.ini","loggerServer");
+        $logServer->process_requests('requestProcessor');
+    }
+
+    function requestProcessor($requestData)
+    {
+        echo "received request".PHP_EOL;
+        print_r($requestData);
+        fwrite($logFile, $requestData);
+    }
+}
+
+// Will be used for a distibuted logger
+class LoggerServer
+{
+    // Automatically called when creating an object from a class
+    function __construct()
+    {
+
+    }
+}
+
+// Will be used for a distibuted logger
+class LoggerClient
+{
+    // Automatically called when creating an object from a class
+    function __construct()
+    {
+
+    }
+}
+?>
