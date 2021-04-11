7ab43fc4ec6b5c8fe89a5fc8e2d1602ed64cbc2d
Michelle Nunez <56059162+mpn6@users.noreply.github.com>
2021-04-11 16:37:27 -0400

    pages with the functions working across rabbitmq

---
+++ pages/chosenLeague.php
@@ -0,0 +1,37 @@
+<?php
+include('path.inc');
+include('get_host_info.inc');
+include('rabbitMQLib.inc');
+include('logger.php'); // "Importing" logger.php
+include('../IT-490-Spring-2021/testRabbitMQClient.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+session_start();
+
+$username = $_SESSION["username"];
+$password = $_SESSION["password"];
+$userID = $_SESSION["userID"];
+$firstname = $_SESSION["firstname"];
+$lastname = $_SESSION["lastname"];
+
+$leagueName = $_POST["leagueName"];
+$leagueID = getLeagueID($leagueName);
+
+$teamID = getTeamID($userID, $leagueID);
+$teamName = getTeamName($teamID);
+
+$_SESSION["teamID"] = $teamID;
+$_SESSION["leagueName"] = $leagueName;
+$_SESSION["leagueID"] = $leagueID;
+$_SESSION["teamName"] = $teamName;
+
+if (! $_SESSION["logged"]){
+    header("Location: login.html");
+    exit();
+}
+
+header("Location: myteam.php");
+
+?>
---
+++ pages/login.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+include('path.inc');
+include('get_host_info.inc');
+include('rabbitMQLib.inc');
+include('logger.php'); // "Importing" logger.php
+include('../IT-490-Spring-2021/testRabbitMQClient.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+if (authentication($username,$password)){
+	$userID = getUserId($username, $password);
+	$firstname = getFirstName($userID);
+	$lastname = getLastName($userID);
+
+	$_SESSION["logged"] = true;
+    $_SESSION["username"] = $username;
+    $_SESSION["password"] = $password;
+    $_SESSION["userID"] = $userID;
+	$_SESSION["firstname"] = $firstname;
+	$_SESSION["lastname"] = $lastname;
+
+    header("Location: chooseleague.php");
+    exit();
+
+}else{
+	$message = "Wrong username or password";
+         echo "<script type='text/javascript'>alert('$message');</script>";
+        header("refresh: 0.00001, url = 'login.html'");
+        exit();
+}
+
+?>
